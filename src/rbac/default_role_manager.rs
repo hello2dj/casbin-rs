693d7636de87fd57cfcb65cb60e42e6e246e1f57
Sebastien Duquette <sduquette@devolutions.net>
2019-01-17 12:45:53 -0500

    Implement DefaultRoleManager::delete_link

--- src/rbac/default_role_manager.rs
+++ src/rbac/default_role_manager.rs
@@ -17,9 +17,9 @@ impl RoleManager for DefaultRoleManager {
         Ok(self.all_roles.clear())
     }
 
-    // Add a link such that `name1` inherits the role `name2`.
-    // 
-    // `domain` is a prefix to the roles.
+    /// Add a link such that `name1` inherits the role `name2`.
+    ///
+    /// `domain` is a prefix to the roles.
     fn add_link(&mut self, name1: &str, name2: &str, domain: Option<&str>) -> Result<(), Error> {
         let (name1, name2) = DefaultRoleManager::get_names_with_domain(name1, name2, domain);
 
@@ -31,11 +31,20 @@ impl RoleManager for DefaultRoleManager {
         Ok(())
     }
 
+    /// Delete the inheritance link between `name1` and  `name2`.
+    ///
+    /// `domain` is a prefix to the roles.
     fn delete_link(&mut self, name1: &str, name2: &str, domain: Option<&str>) -> Result<(), Error> {
-        unimplemented!()
+        let (name1, name2) = DefaultRoleManager::get_names_with_domain(name1, name2, domain);
+        
+        let role1 = self.get_role(&name1).ok_or(Error::MissingRole(name1.clone()))?;
+        let role2 = self.get_role(&name2).ok_or(Error::MissingRole(name2.clone()))?;
+        
+        role1.borrow_mut().delete_role(role2);
+        Ok(())
     }
 
-    // Return true if `name1` inherits the role `name2`.
+    /// Return true if `name1` inherits the role `name2`.
     fn has_link(&self, name1: &str, name2: &str, domain: Option<&str>) -> Result<bool, Error> {
         let (name1, name2) = DefaultRoleManager::get_names_with_domain(name1, name2, domain);
         
@@ -134,8 +143,31 @@ mod tests {
         assert_eq!(manager.has_link("u3", "g1", None).unwrap(), false);
         assert_eq!(manager.has_link("u3", "g2", None).unwrap(), true);
         assert_eq!(manager.has_link("u3", "g3", None).unwrap(), false);
+        assert_eq!(manager.has_link("u4", "g1", None).unwrap(), false);
         assert_eq!(manager.has_link("u4", "g2", None).unwrap(), true);
         assert_eq!(manager.has_link("u4", "g3", None).unwrap(), true);   
-    
+
+        manager.delete_link("g1", "g3", None).unwrap();
+        manager.delete_link("u4", "g2", None).unwrap();
+        
+        // Current role inheritance tree after deleting the links:
+        //             g3    g2
+        //               \     \
+        //          g1    u4    u3
+        //         /  \
+        //       u1    u2
+
+        assert_eq!(manager.has_link("u1", "g1", None).unwrap(), true);
+        assert_eq!(manager.has_link("u1", "g2", None).unwrap(), false);
+        assert_eq!(manager.has_link("u1", "g3", None).unwrap(), false);
+        assert_eq!(manager.has_link("u2", "g1", None).unwrap(), true);
+        assert_eq!(manager.has_link("u2", "g2", None).unwrap(), false);
+        assert_eq!(manager.has_link("u2", "g3", None).unwrap(), false);
+        assert_eq!(manager.has_link("u3", "g1", None).unwrap(), false);
+        assert_eq!(manager.has_link("u3", "g2", None).unwrap(), true);
+        assert_eq!(manager.has_link("u3", "g3", None).unwrap(), false);
+        assert_eq!(manager.has_link("u4", "g1", None).unwrap(), false);
+        assert_eq!(manager.has_link("u4", "g2", None).unwrap(), false);
+        assert_eq!(manager.has_link("u4", "g3", None).unwrap(), true);
     }
 }
