c2e8bd136f6793ac1e75b884c7458d74f27c368b
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-01-18 14:28:33 -0500

    casbin-rs: update policy.rs

--- src/policy.rs
+++ src/policy.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 use crate::error::Error;
 use crate::model::Model;
 use crate::rbac::RoleManager;
+use crate::assertion::Assertion;
 
 impl Model {
     pub fn build_role_links(&mut self, role_manager: &mut Box<RoleManager>) -> Result<(), Error> {
@@ -41,11 +44,11 @@ impl Model {
         unimplemented!()
     }
 
-    pub fn has_policy(&self, sec: &str, ptype: &str, rule: Vec<String>) -> bool {
+    pub fn has_policy(&self, sec: &str, ptype: &str, rule: &Vec<String>) -> bool {
         if let Some(sec_map) = self.data.get(sec) {
             if let Some(assertion) = sec_map.get(ptype) {
                 for a_rule in &assertion.policy {
-                    if a_rule == &rule {
+                    if a_rule == rule {
                         return true;
                     }
                 }
@@ -54,12 +57,38 @@ impl Model {
         false
     }
 
-    pub fn add_policy(&mut self, sec: &str, ptype: &str, rule: Vec<String>) -> Result<bool, Error> {
-        unimplemented!()
+    pub fn add_policy(&mut self, sec: &str, ptype: &str, rule: Vec<String>) -> bool {
+        if !self.has_policy(sec, ptype, &rule) {
+            if !self.data.contains_key(sec) {
+                let sec_map: HashMap<String, Assertion> = HashMap::new();
+                self.data.insert(sec.to_string(), sec_map);
+            }
+
+            let sec_map = self.data.get_mut(sec).unwrap();
+            if !sec_map.contains_key(ptype) {
+                let assertion = Assertion::new();
+                sec_map.insert(ptype.to_string(), assertion);
+            }
+
+            let assertion = sec_map.get_mut(ptype).unwrap();
+            assertion.policy.push(rule);
+
+            true;
+        }
+        false
     }
 
-    pub fn remove_policy(&mut self, sec: &str, ptype: &str, rule: Vec<String>) -> Result<bool, Error> {
-        unimplemented!()
+    pub fn remove_policy(&mut self, sec: &str, ptype: &str, rule: Vec<String>) -> bool {
+        if let Some(sec_map) = self.data.get_mut(sec) {
+            if let Some(assertion) = sec_map.get_mut(ptype) {
+                let index = assertion.policy.iter().position(|x| *x == rule);
+                if let Some(index) = index {
+                    assertion.policy.remove(index);
+                    return true;
+                }
+            }
+        }
+        false
     }
 
     pub fn remove_filtered_policy(
@@ -77,7 +106,22 @@ impl Model {
         sec: &str,
         ptype: &str,
         field_index: i32,
-    ) -> Result<Vec<String>, Error> {
-        unimplemented!()
+    ) -> Vec<String> {
+        let mut values: Vec<String> = Vec::new();
+
+        for (_, sec_map) in &self.data {
+            for (_, assertion) in sec_map {
+                for rules in &assertion.policy {
+                    for rule in rules {
+                        values.push(rule.to_string());
+                    }
+                }
+            }
+        }
+
+        values.sort();
+        values.dedup();
+
+        return values;
     }
 }
