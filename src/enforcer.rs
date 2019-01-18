9fba672a6438f30cbcd197c0f49d50fe34853947
Sebastien Duquette <sduquette@devolutions.net>
2019-01-18 16:08:49 -0500

    Implement initialization of Enforcer

--- src/enforcer.rs
+++ src/enforcer.rs
@@ -15,17 +15,49 @@ pub struct Enforcer {
     adapter: Box<Adapter>,
     role_manager: Box<RoleManager>,
     effector: Box<Effector>,
+    auto_build_role_links: bool,
 }
 
 impl Enforcer {
+    /// Create an instance of an Enforcer from a `model` and `policy`.
     pub fn new<P: 'static + Adapter>(model: &Path, policy: P) -> Result<Self, Error> {
-        Ok(Enforcer {
+        let mut enforcer = Enforcer {
             model: Model::new(model)?,
             model_path: PathBuf::from(model),
             function_map: get_function_map(),
             adapter: Box::new(policy),
             role_manager: Box::new(DefaultRoleManager::new(10)),
             effector: Box::new(DefaultEffector::new()),
-        })
+            auto_build_role_links: true,
+        };
+
+        enforcer.load_policy()?;
+
+        Ok(enforcer)
     }
+
+    /// Rebuild the role inheritance relations.
+    fn build_role_links(&mut self) -> Result<(), Error> {
+        self.role_manager.clear()?;
+        self.model.build_role_links(&mut self.role_manager)?;
+        Ok(())
+    }
+
+    /// Reload the policy from source.
+    fn load_policy(&mut self) -> Result<(), Error> {
+        self.model.clear_policy();
+        self.adapter.load_policy(&mut self.model)?;
+
+        if self.auto_build_role_links {
+            self.build_role_links()?;
+        }
+
+        Ok(())
+    }
+
+    /// Decide whether `subject` can access `object` with the operation `action`.
+    pub fn enforce(subject: &str, object: &str, action: &str) -> Result<bool, Error> {
+        unimplemented!()
+    }
+
 }
