53c30b39b6281f9d1ff41828b7b2724d53a37e33
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-01-18 11:59:45 -0500

    casbin-rs: update policy.rs

--- src/policy.rs
+++ src/policy.rs
@@ -4,23 +4,54 @@ use crate::rbac::RoleManager;
 
 impl Model {
     pub fn build_role_links(&mut self, role_manager: &mut Box<RoleManager>) -> Result<(), Error> {
-        unimplemented!()
+        if let Some(g) = self.data.get_mut("g") {
+            for (name, assertion) in g.iter_mut() {
+                assertion.build_role_links(role_manager)?;
+            }
+        }
+        Ok(())
     }
 
     pub fn clear_policy(&mut self) -> Result<(), Error> {
+        if let Some(p) = self.data.get_mut("p") {
+            p.clear();
+        }
+        if let Some(g) = self.data.get_mut("g") {
+            g.clear();
+        }
+        Ok(())
+    }
+
+    pub fn get_policy(&mut self, sec: &str, ptype: &str) -> Option<Vec<Vec<String>>> {
+        if let Some(sec_map) = self.data.get(sec) {
+            if let Some(assertion) = sec_map.get(ptype) {
+                Some(assertion.policy.clone()); // FIXME: check if we need a reference, or a copy
+            }
+        }
+        None
+    }
+
+    pub fn get_filtered_policy(
+        &mut self,
+        sec: &str,
+        ptype: &str,
+        field_index: i32,
+        field_values: Vec<String>,
+    ) -> Option<Vec<Vec<String>>> {
         unimplemented!()
     }
 
-    pub fn get_policy(&mut self, sec: &str, ptype: &str) -> Result<Vec<Vec<String>>, Error> {
-        unimplemented!()
-    }
-
-    pub fn get_filtered_policy(&mut self, sec: &str, ptype: &str, field_index: i32, field_values: Vec<String>) -> Result<Vec<Vec<String>>, Error> {
-        unimplemented!()
-    }
-
-    pub fn has_policy(&self, sec: &str, ptype: &str, rule: Vec<String>) -> Result<bool, Error> {
-        unimplemented!()
+    pub fn has_policy(&self, sec: &str, ptype: &str, rule: Vec<String>) -> bool {
+        if let Some(sec_map) = self.data.get(sec) {
+            if let Some(assertion) = sec_map.get(ptype) {
+                for a_rule in &assertion.policy {
+                    if a_rule == &rule {
+                        return true;
+                    }
+                }
+            }
+        }
+        false
     }
 
     pub fn add_policy(&mut self, sec: &str, ptype: &str, rule: Vec<String>) -> Result<bool, Error> {
@@ -31,11 +62,22 @@ impl Model {
         unimplemented!()
     }
 
-    pub fn remove_filtered_policy(&mut self, sec: &str, ptype: &str, field_index: i32, field_values: Vec<String>) -> Result<bool, Error> {
+    pub fn remove_filtered_policy(
+        &mut self,
+        sec: &str,
+        ptype: &str,
+        field_index: i32,
+        field_values: Vec<String>,
+    ) -> Result<bool, Error> {
         unimplemented!()
     }
 
-    pub fn get_values_for_field_in_policy(&self, sec: &str, ptype: &str, field_index: i32) -> Result<Vec<String>, Error> {
+    pub fn get_values_for_field_in_policy(
+        &self,
+        sec: &str,
+        ptype: &str,
+        field_index: i32,
+    ) -> Result<Vec<String>, Error> {
         unimplemented!()
     }
 }
